75965dddb31aca408045aec5206bcbd6c16f7c4f
Miroslav Tsarev <mtzarev@gmail.com>
2024-10-13 21:56:51 +0300

    Update ExtractParenthesis.js

--- ExtractParenthesis.js
+++ ExtractParenthesis.js
@@ -3,13 +3,13 @@ let text = document.getElementById(`content`).textContent;
 
 const regex = /\((.+?)\)/gm;
 const matches = [...text.match(regex)];
-let finalArr = []
+let finalArr = [];
 for(let i=0; i<matches.length; i++){
-    let word= matches[i]
-    let correctWrod = word.slice(1, word.length-1)
-    finalArr.push(correctWrod)
+    let word= matches[i];
+    let correctWrod = word.slice(1, word.length-1);
+    finalArr.push(correctWrod);
 }
-let textToShow=finalArr.join(`; `)
+let textToShow=finalArr.join(`; `);
 console.log(textToShow);
 
 }
